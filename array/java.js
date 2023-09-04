b71a57ae3e637eb8168b85dc1d0b4b48ddd30763
chiomalovet <110299085+chiomalovet@users.noreply.github.com>
2023-09-04 10:14:28 +0100

    Delete array directory

--- array/java.js
+++
@@ -1,8 +0,0 @@
-var cars = "toyota, honda, benz, BMW, mitsubish,hyundai, volvo, opel, arton,martins,andi,buntley,lamboghini,innoson,peugeot, ferari bugatti,porsche, royce roys, dodge, lexus,acura";
-var soon = prompt('input character');
-var result = cars.search(check);
-if (result == -1) {
-    alert('not found')
-} else {
-    alert(cars + 'it is DB');
-}
