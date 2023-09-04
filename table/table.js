b243f0a96b9fa45581ae45d75ba2fdf51bf39176
chiomalovet <110299085+chiomalovet@users.noreply.github.com>
2023-09-04 10:30:37 +0100

    Delete table directory

--- table/table.js
+++
@@ -1,7 +0,0 @@
-function good() {
-    document.getElementById('num').value
-    document.getElementById('numb').value
-    document.getElementById('add').value
-    document.getElementById('sub').value
-    document.getElementById('multi').value
-}
